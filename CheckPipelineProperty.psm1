7de537e6634d748079c309be0d0400028485152a
Chirag Nagrekar <chiragce17@gmail.com>
2020-10-14 19:32:13 +0530

    Add files via upload

---
+++ CheckPipelineProperty.psm1
@@ -0,0 +1,26 @@
+﻿function Check-PipeLineProperty{
+   param(
+      [Parameter(Mandatory=$true)]
+      [string[]]$commands
+   )
+
+   try{
+       foreach($C in $commands){
+    
+     Write-Output "`n******************************************************************************************`n"
+     Write-Output "`n$c Pipeline Properties`n"
+
+       (Get-Command $C).ParameterSets.parameters | `
+       Where {($_.ValuefromPipeline -eq 'True') -or ($_.ValueFromPipelineByPropertyName -eq 'True')} | `
+       Select Name,ParameterType, IsMandatory,ValueFromPipeline,ValueFromPipelineByPropertyName | Sort-Object ValueFromPipeline -Descending | ft -AutoSize   
+    }
+   }
+
+   catch{
+      Write-Host $_.Exception.Message -BackgroundColor DarkRed
+   }
+
+   
+
+}
+
